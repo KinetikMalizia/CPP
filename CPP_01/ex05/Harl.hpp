b97e9f44c19348f7eb4446993df488d6b6ea1e47
KinetikMalizia <fmalizia@student.42lausanne.ch>
2023-01-03 17:02:47 +0100

    Harl

--- CPP_01/ex05/Harl.hpp
+++ CPP_01/ex05/Harl.hpp
@@ -1,4 +1,14 @@
 #ifndef HARL_HPP
 # define HARL_HPP
 
+class Harl
+{
+	private:
+
+	public:
+			Harl();
+			~Harl();
+			
+};
+
 #endif
---
+++ CPP_01/ex05/main.cpp
@@ -0,0 +1,6 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	
+}
