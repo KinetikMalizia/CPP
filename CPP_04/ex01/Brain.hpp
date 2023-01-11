bebce046057dc5f503cb982b8020e8bae14108be
Fabio Malizia <fmalizia@c2r7s2.42lausanne.ch>
2023-01-11 11:59:28 +0100

    started 04ex01

--- CPP_04/ex01/Brain.hpp
+++ CPP_04/ex01/Brain.hpp
@@ -1,38 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmalizia <fmalizia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 11:54:48 by fmalizia          #+#    #+#             */
-/*   Updated: 2023/01/11 11:55:48 by fmalizia         ###   ########.ch       */
+/*   Created: 2023/01/11 11:55:51 by fmalizia          #+#    #+#             */
+/*   Updated: 2023/01/11 11:59:12 by fmalizia         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WRONGANIMAL_HPP
-# define WRONGANIMAL_HPP
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
 
 # include <iostream>
 # include <string>
 
-class WrongAnimal {
-	protected:
-		std::string	_type;
-
+class Brain
+{
+	private:
+			std::string	_ideas[100];
 	public:
-		WrongAnimal(std::string type = "wrong");
-		WrongAnimal( WrongAnimal const & src );
-		virtual ~WrongAnimal( void );
-
-		WrongAnimal	&operator=( WrongAnimal const & src );
-
-		void		set_type( std::string const value );
-
-		std::string	get_type( void ) const;
-
-		virtual void makeSound() const;
-
+			Brain();
+			Brain(Brain &og);
+			~Brain();
 };
 
 #endif
